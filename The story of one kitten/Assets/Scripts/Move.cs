14b1a48195d4d3942f6f7dd4e56c9ac72602c85c
GuceAlkaTruse <60811855+GuceAlkaTruse@users.noreply.github.com>
2020-02-26 17:43:16 +0200

    first commit

---
+++ The story of one kitten/Assets/Scripts/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+            _animator.SetInteger("TransitionCheck", 2);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+            _animator.SetInteger("TransitionCheck", 2);
+        }
+        else
+        {
+            _animator.SetInteger("TransitionCheck", 1);
+        }
+    }
+}
